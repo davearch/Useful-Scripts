ef92f1601b1713593ce152f42e5867a62b035841
David Archuleta Jr <darchuletajr@gmail.com>
2021-12-14 10:43:34 -0600

    bash sed script typo

--- bash_sed_directory.sh
+++ bash_sed_directory.sh
@@ -1,5 +1,5 @@
 # useful to find a file of a certain type, then run search and replace on it.
-find . -type f -exec bash -c '[[ "$( file -bi "$1" )" == */html* ]]' bash {} \; -exec sed -i -e "s/bootstrap.bundle.min.js/boostrap.bundle.js/g" {} \;
+find . -type f -exec bash -c '[[ "$( file -bi "$1" )" == */html* ]]' bash {} \; -exec sed -i -e "s/bootstrap.bundle.min.js/bootstrap.bundle.js/g" {} \;
 
 # alternatively (from https://unix.stackexchange.com/questions/483871/how-to-find-files-by-file-type):
 find . -type f -exec bash -c '
